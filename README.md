cec64474117ef213fec3aa1940e2d080ca7e2bba
Rob Larsen <rob@octarine.com>
2016-01-28 17:34:27 +0000

    Initial code for jenkins CI server

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# dstl-lighthouse-builder
-# dstl-lighthouse-builder
+# Infrastructure provisioning for dstl-lighthouse
+
+## Description
+
+Code for provision and deployment for various components of the dstl-lighthouse project
+
+
+## Jenkins
+
+Bootstraps jenkins for the dstl-lighthouse project
+
+
---
+++ jenkins/bootstrap.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Make sure only root can run our script
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+echo "Installing IUS release repo"
+
+curl -s https://setup.ius.io/ | bash
+
+echo "Install Ansible"
+
+yum -y install ansible
+
+echo "Install ansible playbook for jenkins"
+
+ansible-galaxy install geerlingguy.jenkins
+
+echo "Install jenkins"
+
+ansible-playbook bootstrap.yml
