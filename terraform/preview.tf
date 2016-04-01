1851c348b021764f57731d40530d47c9d83133ce
Michael Allen <michael@michaelallen.io>
2016-04-01 15:04:15 +0100

    Remove old centos boxes from preview and switch urls to point to rhel boxes

--- terraform/preview.tf
+++ terraform/preview.tf
@@ -31,34 +31,12 @@ resource "aws_security_group" "allow_office_ip" {
   }
 }
 
-resource "aws_instance" "jenkins-ci" {
-    ami = "${var.centos_box}"
-    instance_type = "t2.micro"
-    key_name = "deploy"
-    availability_zone = "${var.availability_zone}"
-    security_groups = ["${aws_security_group.allow_office_ip.name}"]
-    tags {
-      Name = "jenkins-ci"
-    }
-}
-
-resource "aws_instance" "lighthouse-app" {
-    ami = "${var.centos_box}"
-    instance_type = "t2.micro"
-    key_name = "deploy"
-    availability_zone = "${var.availability_zone}"
-    security_groups = ["${aws_security_group.allow_office_ip.name}"]
-    tags {
-      Name = "lighthouse-app"
-    }
-}
-
 resource "aws_eip" "jenkins-public-ip" {
-  instance = "${aws_instance.jenkins-ci.id}"
+  instance = "${aws_instance.redhat-jenkins-ci.id}"
 }
 
 resource "aws_eip" "lighthouse-public-ip" {
-  instance = "${aws_instance.lighthouse-app.id}"
+  instance = "${aws_instance.redhat-lighthouse-app.id}"
 }
 
 resource "aws_route53_zone" "primary" {
@@ -70,7 +48,7 @@ resource "aws_route53_record" "ci" {
   name = "ci.lighthouse.pw"
   type = "A"
   ttl = "60"
-  records = ["${aws_eip.jenkins-public-ip.public_ip}"]
+  records = ["${aws_eip.redhat-jenkins-public-ip.public_ip}"]
 }
 
 resource "aws_route53_record" "www" {
@@ -78,13 +56,13 @@ resource "aws_route53_record" "www" {
   name = "www.lighthouse.pw"
   type = "A"
   ttl = "60"
-  records = ["${aws_eip.lighthouse-public-ip.public_ip}"]
+  records = ["${aws_eip.redhat-lighthouse-public-ip.public_ip}"]
 }
 
 output "internal_ip" {
-    value = "${aws_instance.lighthouse-app.private_ip}"
+    value = "${aws_instance.redhat-lighthouse-app.private_ip}"
 }
 
 output "external_ip" {
-    value = "${aws_instance.lighthouse-app.public_ip}"
+    value = "${aws_instance.redhat-lighthouse-app.public_ip}"
 }
